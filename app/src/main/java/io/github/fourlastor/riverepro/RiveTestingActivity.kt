f75c30c655e9babc0213b83047ec33536aa414f2
Daniele Conti <fourlastor@users.noreply.github.com>
2021-11-04 18:04:13 +0000

    Initial commit

---
+++ app/src/main/java/io/github/fourlastor/riverepro/RiveTestingActivity.kt
@@ -0,0 +1,41 @@
+package io.github.fourlastor.riverepro
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import app.rive.runtime.kotlin.core.Alignment
+import io.github.fourlastor.riverepro.databinding.ActivityRiveTestingBinding
+
+/** Sandbox to test Rive animations. */
+class RiveTestingActivity : AppCompatActivity() {
+
+  private lateinit var binding: ActivityRiveTestingBinding
+  private var currentAnimationIndex: Int? = null
+
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    hideActionBar()
+    binding = ActivityRiveTestingBinding.inflate(layoutInflater)
+
+    setContentView(binding.root)
+
+    binding.animationView.setRiveResource(
+      resId = R.raw.off_road_car_blog,
+      alignment = Alignment.CENTER,
+    )
+  }
+
+  private fun hideActionBar() {
+    supportActionBar?.run {
+      // make sure bar is hidden
+      setBackgroundDrawable(null)
+      setDisplayShowCustomEnabled(false)
+      setDisplayShowTitleEnabled(false)
+      setDisplayUseLogoEnabled(false)
+      setDisplayShowHomeEnabled(false)
+      setDisplayHomeAsUpEnabled(false)
+      elevation = 0f
+      hide()
+    }
+  }
+}
